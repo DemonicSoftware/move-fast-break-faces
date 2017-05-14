2c1ed786a9c904c69f0df21950de93e21b9cc29b
rainierneri <rainier120@gmail.com>
2017-05-15 00:01:41 +1000

    Fixed walking angle. Player should always face mouse direction and no more strange movement angles.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -69,6 +69,6 @@ public class PlayerControl : MonoBehaviour
 		var screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
 		var offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
 		var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.Euler(0, 0, angle);
+		transform.rotation = Quaternion.Euler(0, 0, angle - 90);
 	}
 }
