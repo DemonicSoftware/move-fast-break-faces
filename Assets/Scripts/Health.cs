6d23b6a8bc206c53849dc79afdd339bc0ddfd797
Jonathan Moallem <jdmoallem@gmail.com>
2017-05-16 12:00:21 +1000

    Added health class and gutted PlayerController (renamed it too)

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health {
+
+	private int health;
+	private int baseHealth;
+
+
+	public Health(int startingHealth) {
+		baseHealth = startingHealth;
+		health = baseHealth;
+	}
+
+	public void dealDamage(int damage) {
+		health -= damage;
+	}
+
+	void restoreHealth(int healing) {
+		health += healing;
+	}
+
+	public void refillHealth() {
+		health = baseHealth;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Health health;
+	public Text healthText;
+
+	void Start () {
+        health = new Health(5);
+        healthText.text = health.getHealth().ToString();
+	}
+
+
+	void update() {
+	}
+
+	void FixedUpdate() {
+        if (Input.GetKey(KeyCode.X)) {
+            health.dealDamage(1);
+        }
+
+        if (Input.GetKey(KeyCode.Z)) {
+            health.refillHealth();
+        }
+        healthText.text = health.getHealth().ToString();
+	}
+}
