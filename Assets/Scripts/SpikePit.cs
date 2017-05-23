9e98f07ce7293cb24472d2bd59d6d7a7fab41b9c
adam <adambursill@hotmail.com>
2017-05-23 14:33:45 +1000

    fleshed out the level 4 map abit, added spike traps which slow you down also added fire animation for the fire piles just to look pretty

---
+++ Assets/Scripts/SpikePit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikePit : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if(collision.CompareTag("Player"))
+        {        
+            collision.GetComponent<Rigidbody2D>().drag = 60;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<Rigidbody2D>().drag = 15;
+        }
+    }
+
+
+
+
+}
