5d2579445cb067d7fa91c6f9af0113ab907e7554
Jonathan Moallem <jdmoallem@gmail.com>
2017-05-28 16:20:28 +1000

    Added to main menu, can go to each level using LevelSwitcher

---
+++ Assets/Scripts/LevelSwitcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSwitcher : MonoBehaviour {
+
+	public void LoadLevelOne() {
+		Application.LoadLevel("Level 1");
+	}
+
+	public void LoadLevelTwo() {
+		Application.LoadLevel("Level 2");
+	}
+
+	public void LoadLevelThree() {
+		Application.LoadLevel("Level 3");
+	}
+
+	public void LoadLevelFour() {
+		Application.LoadLevel("Level 4");
+	}
+
+	public void LoadLevelFive() {
+		Application.LoadLevel("Level 5");
+	}
+}
