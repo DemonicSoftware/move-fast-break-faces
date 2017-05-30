bb0c80d484ea205ef8c2d252531ae8df74d47ced
LIUJUNHENG-PC\lenovo <910728069@qq.com>
2017-05-30 19:09:43 +0800

    add Health and Damage function for the BOSS, ranged enemy won't has melee damage to the hero now

--- Assets/Scripts/BossAttack.cs
+++ Assets/Scripts/BossAttack.cs
@@ -8,9 +8,14 @@ public class BossAttack : MonoBehaviour {
     public float shootingRate = 0.25f;
     private float shootCooldown = 0;
     public int damage = 1;
+    public int HP = 20;
+    private bool dead = false;
+    private bool isEnemy = true;
+    private Animator anim;
     // Use this for initialization
     void Start () {
         lance.GetComponent<Collider2D>().enabled = true;
+        anim = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -39,6 +44,54 @@ public class BossAttack : MonoBehaviour {
         {
             shootCooldown = shootingRate;
             lance.GetComponent<Collider2D>().enabled = true;
+        }
+    }
+
+    void Damage(int damageCount)
+    {
+        HP -= damageCount;
+        if(HP <= 0)
+        {
+            Destroy(gameObject, 2f);
+            dead = true;
+            if (GetComponent<Animator>() != null)
+                anim.SetBool("dead", true);
+            if (GetComponent<FollowPlayer>() != null)
+                GetComponent<FollowPlayer>().enabled = false;
+            if (GetComponent<Collider2D>() != null)
+                GetComponent<Collider2D>().enabled = false;
+            if (GetComponent<RangedEnemyMovement>() != null)
+                GetComponent<RangedEnemyMovement>().enabled = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        // Is this a shot?
+        RangeAttack range = otherCollider.gameObject.GetComponent<RangeAttack>();
+        MeleeAttack melee = otherCollider.gameObject.GetComponent<MeleeAttack>();
+        if (range != null)
+        {
+            // Avoid friendly fire
+            if (range.isEnemy != isEnemy)
+            {
+                Damage(range.damage);
+
+                // Destroy the shot
+                Destroy(range.gameObject);
+                // Remember to always target the game object, otherwise you will just remove the script
+            }
+        }
+
+        if (melee != null)
+        {
+            // Avoid friendly fire
+            if (melee.isEnemy != isEnemy)
+            {
+                Damage(melee.damage);
+
+                //Destroy(melee.gameObject); 
+            }
         }
     }
 }
