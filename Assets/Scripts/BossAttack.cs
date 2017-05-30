3383357a26320df892741ed7d98f3de0cfe90853
LIUJUNHENG-PC\lenovo <910728069@qq.com>
2017-05-30 14:22:25 +0800

    added the initial attacking collider and script for the boss

---
+++ Assets/Scripts/BossAttack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAttack : MonoBehaviour {
+    public GameObject lance;
+    public bool CanAttack = true;
+    public float shootingRate = 0.25f;
+    private float shootCooldown = 0;
+    public int damage = 1;
+    // Use this for initialization
+    void Start () {
+        lance.GetComponent<Collider2D>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (CanAttack == false)
+        {
+            if (shootCooldown > 0)
+            {
+                shootCooldown -= Time.deltaTime;
+            }
+            if (shootCooldown <= 0f)
+            {
+                CanAttack = true;
+                lance.GetComponent<Collider2D>().enabled = false;
+            }
+            else
+            {
+                CanAttack = false;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        if (CanAttack)
+        {
+            shootCooldown = shootingRate;
+            lance.GetComponent<Collider2D>().enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/BossLance.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossLance : MonoBehaviour {
+    public int damage = 1;
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<Collider2D>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GetComponent<Collider2D>().enabled = false;
+    }
+}
