ac28d0c6908231d04dad7c416e717f61c0e46602
rainierneri <rainier120@gmail.com>
2017-06-03 10:43:18 +1000

    Almost completely removed Ranged attack delay.

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -9,7 +9,7 @@ public class Combat : MonoBehaviour {
     public GameObject meleeAttackTrigger;
     public float shootingRate = 0.0f;
     private float shootCooldown = 0;
-    public bool CanAttack =true;
+    public bool CanAttack = true;
     public Vector2 direction = new Vector2(1, 0);
     public bool gun = false;
     public double gunAmmo = 0;
@@ -23,89 +23,41 @@ public class Combat : MonoBehaviour {
         //meleeAttackTrigger.GetComponent<Collider2D>().enabled = false;
         //meleeAttackTrigger.GetComponent<SpriteRenderer>().enabled = false;
     }
-
-    void Update() {
-        if (CanAttack == false)
-        {
-            if (shootCooldown > 0)
-            {
-                shootCooldown -= Time.deltaTime;
-            }
-            if (shootCooldown <= 0f)
-            {
-                CanAttack = true;
-               //meleeAttackTrigger.GetComponent<Collider2D>().enabled = false;
-                //meleeAttackTrigger.GetComponent<SpriteRenderer>().enabled = false;
-            }
-            else
-            {
-                CanAttack = false;
-            }
-        }
-    }
-
-
+		
     public void Attack(bool isEnemy) {
-        if (CanAttack) {
-            if (currentWeapon == "gun") {
+        if (currentWeapon == "gun") {
+			
+			if (gunAmmo <= 0) {
+				gun = false;
+			} else {
+				gun = true;
+			}
 
-				if (gunAmmo <= 0) {
-					gun = false;
-				} else {
-					gun = true;
+			if (gun == true) {
+				shootCooldown = shootingRate;
+				// Create a new shot
+				var shotTransform = Instantiate (Shots) as Transform;
+			
+				// Assign position
+				shotTransform.position = transform.position;
+				RangeAttack shot = shotTransform.gameObject.GetComponent<RangeAttack> ();
+				shot.direction = this.direction;
+
+				// The is enemy property
+				if (shot != null) {
+					shot.isEnemy = isEnemy;
 				}
+					
+				gunAmmo -= 1;
 
-				if (gun == true) {
-					shootCooldown = shootingRate;
-					CanAttack = false;
-					// Create a new shot
-					var shotTransform = Instantiate (Shots) as Transform;
-
-					// Assign position
-					shotTransform.position = transform.position;
-
-					RangeAttack shot = shotTransform.gameObject.GetComponent<RangeAttack> ();
-					shot.direction = this.direction;
-
-					// The is enemy property
-					if (shot != null) {
-						shot.isEnemy = isEnemy;
-					}
-
-					gunAmmo -= 1;
-
-				}
-
-            }
-            else if (currentWeapon == "hand")
-            {
-                shootCooldown = shootingRate;
-                CanAttack = false;
-                //meleeAttackTrigger.GetComponent<Collider2D>().enabled = true;
-
-                //meleeAttackTrigger.GetComponent<SpriteRenderer>().enabled = true;
-
-                // The is enemy property
-                // if (handAttack != null) {
-                  //   handAttack.isEnemy = isEnemy;
-                 //}
-            }
-            //else if(currentWeapon == "sword"&& sword == true) {
-
-           // }
-            else {
-                changeWeapon();
-            }
-        }
-    }
-
-    /*public bool CanAttack
-    {
-        get
-        {
-            return shootCooldown <= 0f;
-        }
-    }*/
+			}
+				
+		}
+		else if (currentWeapon == "hand") {
+			shootCooldown = shootingRate;
+		}
+			
+	}
 
     public void changeWeapon() {
 		if (currentWeapon == "hand") {
